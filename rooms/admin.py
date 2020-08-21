3bb5acb2a87ef844b05ecae19401e5acf78edd1b
junji9072 <flasusmaga@gmail.com>
2020-08-21 19:11:43 +0900

    '6강까지 완료'

--- rooms/admin.py
+++ rooms/admin.py
@@ -24,7 +24,74 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),  # 접고 펼수 있는 기능을 해줍니다. 많은 내용을 담고 있을 시 유용합니다.
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    # ordering = ('name','price','bedrooms') 이름이나 가격 룸 개수를 중심으로 리스트 순서를 정렬해줍니다.
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",  # 양이 많으므로 항상 밑에 존재해야합니다.
+        # "host__superhost",
+        # "host__gender",장고 relationship으로 FK로 사용이 가능합니다.
+    )
+    """
+    필터를 통해 나라별 도시별 예약별로 관리자 페이지에서 볼 수 있습니다.
+    list_filter = ("instant_book", "city", "country")
+    """
+
+    """
+    "city"도시의 seo 까지만 써도 검색이 가능한데, icontains라는게 기본으로 되어 있기 때문입니다. insensitive를 포함하며
+    이 말은 대/소문자를 구분하지 않습니다. 정확한 검색을 위해서 = 옵션을 주도록 하겠습니다. host FK로 __로 and 조건을 걸어서 도시로
+    검색이 안될 시 호스트 유저네임으로 검색 가능하도록 했습니다. 또한 ^로 startswith 옵션을 줘서 해당 열의 값이 지정한 문자열을 포함하도록 했습니다.
+    """
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )  # 이 필터는 N:N 관계에서만 작동합니다.
+
+    def count_amenities(self, obj):  # self는 RoomAdmin 클래스이고 obj는 행을 뜻합니다.
+        return obj.amenities.count()
+
+    # count_amenities.short_description = 'hi'로 설명란을 바꿔줄 수 있습니다. functionality이므로 다른 것과 다르게 클릭 불가합니다. 장고가 함수란건 알기 때문입니다!
 
 
 @admin.register(models.Photo)
